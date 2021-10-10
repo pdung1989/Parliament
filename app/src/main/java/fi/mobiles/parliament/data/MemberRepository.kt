f31ef0d0ee85167f696e71d6e8d3b8eaf7c04f85
Dung Tran <pdung1989@gmail.com>
2021-10-10 12:01:31 +0300

    add repository, add name to comment list view

--- app/src/main/java/fi/mobiles/parliament/data/MemberRepository.kt
+++ app/src/main/java/fi/mobiles/parliament/data/MemberRepository.kt
@@ -1,17 +1,17 @@
-//package fi.mobiles.parliament.data
-//
-//import androidx.lifecycle.LiveData
-//
-//class MemberRepository(private val memberDao: MemberDao) {
-//
-//    fun getAll(): LiveData<List<Member>> = memberDao.getAll()
-//
-//    suspend fun insert(member: Member) = memberDao.insert(member)
-//    fun getMember(personNumber: Int): LiveData<Member> = memberDao.getMember(personNumber)
-//
-//    suspend fun insertRating(rating: Rating) = memberDao.insertRating(rating)
-//    suspend fun insertComment(comment: Comment) = memberDao.insertComment(comment)
-//
-//    fun getAllRatings(personNumber: Int): LiveData<List<Double>> = memberDao.getAllRatings(personNumber)
-//    fun getAllComments(personNumber: Int): LiveData<List<Comment>> = memberDao.getAllComments(personNumber)
-//}
+package fi.mobiles.parliament.data
+
+import androidx.lifecycle.LiveData
+
+class MemberRepository(private val memberDao: MemberDao) {
+
+    fun getAll(): LiveData<List<Member>> = memberDao.getAll()
+
+    suspend fun insert(member: Member) = memberDao.insert(member)
+    fun getMember(personNumber: Int): LiveData<Member> = memberDao.getMember(personNumber)
+
+    suspend fun insertRating(rating: Rating) = memberDao.insertRating(rating)
+    suspend fun insertComment(comment: Comment) = memberDao.insertComment(comment)
+
+    fun getAllRatings(personNumber: Int): LiveData<List<Double>> = memberDao.getAllRatings(personNumber)
+    fun getAllComments(personNumber: Int): LiveData<List<Comment>> = memberDao.getAllComments(personNumber)
+}
